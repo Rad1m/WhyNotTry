b539598b6ad7160c1cb5ff5939103eb8d59d169f
Rad1m <radim.simanek@protonmail.ch>
2024-07-01 15:45:17 +0200

    Initial Commit

---
+++ WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Radim Simanek on 01.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
